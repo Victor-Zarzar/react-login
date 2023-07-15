f2aa29f38479bf237c012e5c02b0281c7d058e69
victorzarzar <victorzarzar58@gmail.com>
2023-07-15 11:12:47 -0300

    updates

--- frontend/react-login/src/componentes/Login.jsx
+++ frontend/react-login/src/componentes/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import { RegisterUser } from "../Api";
 
 function Login() {
     
@@ -7,15 +7,17 @@ function Login() {
     const [password, setPassword] = useState("");
 
     const handleLogin = async (event) => {
-        event.preventDefault();
-
-        console.log(email, password);
-
-        // eslint-disable-next-line no-unused-vars
-        const response = await axios.post("http://localhost:5000/login",
-        JSON.stringify({email, password})
-        );
-    }
+      event.preventDefault();
+  
+      try {
+        const response = await RegisterUser(email);
+        console.log(response.data);       
+        setEmail("");
+        setPassword("");
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     return (
       <>
@@ -25,9 +27,9 @@ function Login() {
             <h2>Login</h2>
             <form className='login-form'>
               <input type='email' name='email' placeholder='Email' required
-              onChange={(event) => setEmail(event.target.value)}/>
+              onChange={(event) => setEmail(event.target.value)} value={email}/>
               <input type='password' name='password' placeholder='Password' required
-              onChange={(event) => setPassword(event.target.value)}/>
+              onChange={(event) => setPassword(event.target.value)} value={password}/>
               <button type='submit' className='btn-login'
               onClick={(event) => handleLogin(event)}>Login</button>
             </form>
